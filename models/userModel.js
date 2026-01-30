3db62d9a31f42b99929eb56bed5da66f288e15a8
Raza Aziz <razaaziz9191@gmail.com>
2026-01-30 22:59:35 +0500

    Added email address trim, format validator and lowercase conversion

--- models/userModel.js
+++ models/userModel.js
@@ -9,6 +9,13 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true, // To get unique emails
+    lowercase: true, // To save emails in lowercase
+    trim: true, // To trim any whitespaces (my habit)
+    format: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email address",
+    ],
   },
   password: {
     type: String,
