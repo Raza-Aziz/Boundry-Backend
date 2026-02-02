4518824a22a1cfe7ffa171bed6fa2646a6f2ab5c
Raza Aziz <razaaziz9191@gmail.com>
2026-02-02 23:29:49 +0500

    Updated User model for avatar, phone, listings virtual, and index

--- models/userModel.js
+++ models/userModel.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
   // ! Remember : Fields are set by objects
-  name: {
+  username: {
     type: String,
     required: true,
+    lowercase: true,
   },
   email: {
     type: String,
@@ -12,7 +13,7 @@ const userSchema = mongoose.Schema({
     unique: true, // To get unique emails
     lowercase: true, // To save emails in lowercase
     trim: true, // To trim any whitespaces (my habit)
-    format: [
+    match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please enter a valid email address",
     ],
@@ -30,8 +31,42 @@ const userSchema = mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\+?[1-9][0-9]{7,14}$/, "Please enter a valid Phone number"],
+    // NOTE: Alternative regex format
+    // match: [/^\+?[1-9]\d{1,14}$/, "Please enter a valid Phone number"],
+    trim: true,
+  },
+  avatar: {
+    url: {
+      type: String,
+      default:
+        "https://icons.veryicon.com/png/o/miscellaneous/cust-background-icon/default-avatar-3.png",
+    },
+    publicId: { type: String, default: "" },
+  },
+  // updatedAt: {}
 });
 
-const User = mongoose.Model("User", userSchema);
+userSchema.virtual("listings", {
+  ref: "Listing",
+  count: true,
+  localField: "_id",
+  foreignField: "owner",
+});
+
+// Creating index
+userSchema.index({ email: 1 }, { unique: true }); // email index for normal uses as unique email
+userSchema.index({ username: 1 }, { unique: true }); // username index for speed in login
+userSchema.index({ isAdmin: 1 }); // isAdmin index for admin dashboard
+
+// to include virtuals in res.json(), when sending to frontend
+userSchema.set("toJSON", { virtuals: true });
+userSchema.set("toObject", { virtuals: true });
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
