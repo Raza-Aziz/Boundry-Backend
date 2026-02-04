aa605236a91829497e2b7546038a589ff900b96f
Raza Aziz <razaaziz9191@gmail.com>
2026-02-04 14:31:18 +0500

    Added updating and deleting listing methods

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -60,7 +60,7 @@ export const createListing = async (req, res) => {
 
   if (fields.find((missingField) => !req.body[missingField])) {
     return res.status(400).json({
-      message: `Please fill all required fields :: Missing field ${missingField}`,
+      message: `Please fill all required fields`,
     });
   }
 
@@ -75,6 +75,66 @@ export const createListing = async (req, res) => {
   res.status(201).json(savedListing);
 };
 
-export const deleteListing = async (req, res) => {};
+export const deleteListing = async (req, res) => {
+  // find and delete the listing
+  // req.listing by middleware
+  const listing = req.listing;
 
-export const updateListing = async (req, res) => {};
+  await listing.deleteOne();
+
+  if (listingDeleted) {
+    res.status(200).json({ message: "Listing successfully deleted" });
+  } else {
+    res.status(400).json({ message: "Deleting listing failed" });
+  }
+};
+
+export const updateListing = async (req, res) => {
+  // check if owner (done by middleware)
+  // extract the details to be updated
+  fields = [
+    title,
+    description,
+    price,
+    location,
+    propertyType,
+    bedrooms,
+    bathrooms,
+    areaSqft,
+    images,
+  ];
+
+  if (fields.find((missingField) => !req.body[missingField])) {
+    return res.status(400).json({
+      message: `Please fill all required fields`,
+    });
+  }
+
+  // HACK : No need for that, can do req.listing bcz of middleware instead
+  // const listing = await Listing.findById(req.params.id);
+
+  if (req.listing) {
+    // replace existing with new data
+    req.listing.title = req.body.title || listing.title;
+    req.listing.description = req.body.description || listing.description;
+    req.listing.price = req.body.price || listing.price;
+    req.listing.location = req.body.location || listing.location;
+    req.listing.propertyType = req.body.propertyType || listing.propertyType;
+    req.listing.bedrooms = req.body.bedrooms || listing.bedrooms;
+    req.listing.bathrooms = req.body.bathrooms || listing.bathrooms;
+    req.listing.areaSqft = req.body.areaSqft || listing.areaSqft;
+    // TODO: Check for images
+    // req.listing.images = req.body.images || listing.images
+
+    // the listing will go back for approval
+    req.listing.isApproved = false;
+
+    // re-save the document
+    const updatedListing = await listing.save();
+
+    // send data to frontend
+    res.status(200).json(updatedListing);
+  } else {
+    return res.status(404).json({ message: "Listing not found" });
+  }
+};
