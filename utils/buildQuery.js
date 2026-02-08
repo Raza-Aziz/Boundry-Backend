27adbae50999b4de4b909e6305baf50c6f85054b
Raza Aziz <razaaziz9191@gmail.com>
2026-02-09 00:42:15 +0500

    Implemented query builder for filters and searches

---
+++ utils/buildQuery.js
@@ -0,0 +1,48 @@
+const buildQuery = (userRequests) => {
+  let query = { isApproved: true };
+
+  if ("city" in userRequests) {
+    query["location.city"] = userRequests.city;
+  }
+
+  if ("minPrice" in userRequests || "maxPrice" in userRequests) {
+    query.price = {};
+
+    if (userRequests.minPrice) {
+      query.price.$gte = Number(userRequests.minPrice);
+    }
+    if (userRequests.maxPrice) {
+      query.price.$lte = Number(userRequests.maxPrice);
+    }
+  }
+
+  if (userRequests.search) {
+    // INFO : "i" meaning case-insensitive
+    query.title = { $regex: userRequests.search, $options: "i" };
+  }
+
+  if ("bedrooms" in userRequests) {
+    query.bedrooms = Number(userRequests.bedrooms);
+  }
+
+  if ("minAreaSqft" in userRequests || "maxAreaSqft" in userRequests) {
+    query.areaSqft = {};
+
+    if ("minAreaSqft" in userRequests) {
+      query.areaSqft.$gte = userRequests.minAreaSqft;
+    }
+    if ("maxAreaSqft" in userRequests) {
+      query.areaSqft.$lte = userRequests.maxAreaSqft;
+    }
+  }
+
+  if ("propertyType" in userRequests) {
+    const propertyTypesArray = userRequests.propertyTypes.split(",");
+
+    query.propertyType = { $in: propertyTypesArray };
+  }
+
+  return query;
+};
+
+export default buildQuery;
