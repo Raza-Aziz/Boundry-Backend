2b6b0461ce07f7d83597e986310b22ade54e4c0b
Raza Aziz <razaaziz9191@gmail.com>
2026-02-11 16:00:00 +0500

    Implemented Get all users, change user admin status, and Delete user as Admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -100,19 +100,19 @@ export const deleteUserListing = async (req, res) => {
 };
 
 export const getAllUsers = async (req, res) => {
+  // I think , should include pagination, cause of many users
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const finalLimit = Math.min(limit, 100);
+
+  const skip = (page - 1) * finalLimit;
+
   try {
     const [users, userCount] = await Promise.all([
-      User.find({}),
-      User.estimatedDocumentCount({}),
+      User.find({}).skip(skip).limit(finalLimit).select("-password"),
+      User.countDocuments({}),
     ]);
-
-    // I think , should include pagination, cause of many users
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit);
-
-    const finalLimit = Math.min(limit, 100);
-
-    const skip = (page - 1) * finalLimit;
 
     if (!users) {
       return res.status(404).json({ message: "No users found" });
@@ -127,4 +127,40 @@ export const getAllUsers = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+export const changeUserAdminStatus = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.isAdmin = !user.isAdmin;
+    await user.save();
+
+    res.status(200).json({
+      message: `User is now ${updatedUser.isAdmin ? "an Admin" : "a Regular User"}`,
+      listing: user,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: `User admin status could not be updated :: ${error}` });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+  if (!deletedUser) {
+    return res
+      .status(404)
+      .json({ message: "User could not be found to delete" });
+  }
+
+  res
+    .status(200)
+    .json({ message: "User successfully deleted", user: deletedUser });
 };
