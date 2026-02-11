f6b048e857196aa9518f2aef078671bf9af59199
Raza Aziz <razaaziz9191@gmail.com>
2026-02-11 16:01:04 +0500

    Added Admin User routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,34 @@
 import express from "express";
-import { authorizeAdmin } from "../middleware/auth";
+import { authenticateToken, authorizeAdmin } from "../middleware/auth";
+import {
+  getUserPendingListings,
+  approveUserListing,
+  rejectUserListing,
+  deleteUserListing,
+  getAllUsers,
+  changeUserAdminStatus,
+  deleteUser,
+} from "../controllers/adminController";
 
 const router = express.Router();
 
-router.route("/listings/pending").get(authorizeAdmin, getUserPendingListings);
-router.route("/listings/:id/approve").patch(authorizeAdmin, approveUserListing);
-router.route("/listings/:id/reject").patch(authorizeAdmin, rejectUserListing);
-router.route("/listings/:id").delete(authorizeAdmin, deleteUserListing);
+// ---INFO : ADMIN LISTING ROUTES---
+router
+  .route("/listings/pending")
+  .get(authenticateToken, authorizeAdmin, getUserPendingListings);
+router
+  .route("/listings/:id/approve")
+  .patch(authenticateToken, authorizeAdmin, approveUserListing);
+router
+  .route("/listings/:id/reject")
+  .patch(authenticateToken, authorizeAdmin, rejectUserListing);
+router
+  .route("/listings/:id")
+  .delete(authenticateToken, authorizeAdmin, deleteUserListing);
+
+// ---INFO : ADMIN USER ROUTES---
+router.route("/users").get(authenticateToken, authorizeAdmin, getAllUsers);
+router
+  .route("/users/:id/role")
+  .patch(authenticateToken, authorizeAdmin, changeUserAdminStatus);
+router.route("/users/:id").patch(authenticateToken, authorizeAdmin, deleteUser);
