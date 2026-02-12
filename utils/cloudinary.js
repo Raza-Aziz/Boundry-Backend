1e4e340e6ba8630b0c8b4cb461164a338a357bb2
Raza Aziz <razaaziz9191@gmail.com>
2026-02-13 01:32:30 +0500

    Built upload service to cloudinary using file path

---
+++ utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been successfully uploaded
+    console.log("File has been successfully uploaded", response.url);
+    return response;
+  } catch (error) {
+    // INFO : removes the locally saved temp file if the upload fails
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
