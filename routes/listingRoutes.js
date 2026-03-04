fb161fbe5b6d33f23a186feae752493262182884
Raza Aziz <razaaziz9191@gmail.com>
2026-03-04 17:30:01 +0500

    Added a controller and route of acquired status

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteListing,
   updateListing,
   getUserListings,
+  toggleAcquiredStatus,
 } from "../controllers/listingController.js";
 import checkOwnership from "../middleware/ownership.js";
 import { authenticateToken } from "../middleware/auth.js";
@@ -15,13 +16,17 @@ const router = express.Router();
 
 router.route("/").get(getAllPublicListings);
 router.post("/", authenticateToken, upload.array("images", 10), createListing);
+router.route("/my-listings").get(authenticateToken, getUserListings);
+
+router
+  .route("/:id/acquired-status")
+  .patch(authenticateToken, checkOwnership, toggleAcquiredStatus);
 
 // TODO: Make a route to GET all pending listings for logged-in user
-router.route("/my-listings").get(authenticateToken, getUserListings);
 router
   .route("/:id")
   .get(getListing) // No need for checkOwnership as normal user won't be able to see then
-  .put(checkOwnership, updateListing)
+  .patch(checkOwnership, updateListing)
   .delete(checkOwnership, deleteListing);
 
 // TODO: Create /stats endpoint using MongoDB Aggregation for Dashboard insights (Module 4.4)
