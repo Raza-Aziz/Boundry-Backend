a14db821a30d0b41aec45dee8b3c540e678e3d7e
Raza Aziz <razaaziz9191@gmail.com>
2026-03-06 22:22:47 +0500

    Changed sameSite from "strict" to "none"

--- utils/jwt.js
+++ utils/jwt.js
@@ -12,7 +12,7 @@ export const generateAccessToken = (res, userId) => {
 
   res.cookie("jwt", accessToken, {
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
     maxAge: 1000 * 60 * 60,
   });
